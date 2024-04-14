6b6778ab26b670ca7f04a59ec7b1248b84e5f966
km <>
2024-04-14 17:39:51 -0400

    updated gitignore, updated build script, added clean script

--- build.sh
+++ build.sh
@@ -3,5 +3,12 @@
 CC=gcc
 FLAGS=(-Wall -Wpedantic -V -0fast -pipe -std=gnu18)
 
+echo "Compiling..."
+sleep 3
 $CC ${FLAGS} -c *.c
+
+echo "Linking..."
+sleep 3
 $CC ${FLAGS} -o pgm *.o
+
+echo "Finished"
